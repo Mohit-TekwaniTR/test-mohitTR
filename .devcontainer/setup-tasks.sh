bb6ab83b3a513222b8b79a4d5d1ed43bac2ed016
Mohit-TekwaniTR <112473013+Mohit-TekwaniTR@users.noreply.github.com>
2023-12-05 20:02:53 +0530

    Update setup-tasks.sh

--- .devcontainer/setup-tasks.sh
+++ .devcontainer/setup-tasks.sh
@@ -1,30 +1,5 @@
-mkdir -p .vscode
-
-cat <<EOL > .vscode/tasks.json
-
-{
-    "version": "2.0.0",
-    "tasks": [
-      {
-        "label": "openFolder",
-        "type": "shell",
-        "command": "code",
-        "args": ["workspace/../content/event_bus.md"],
-        "presentation": {
-          "echo": true,
-          "reveal": "always",
-          "focus": false,
-          "panel": "shared"
-        }
-      }
-    ]
-  }
+FILE_TO_OPEN="workspace/../models/Model.js"
 
 
 
-
-
-EOL
-
-code -r openFolder
-
+code --goto "$FILE_TO_OPEN" --wait --run "$TASK_NAME"

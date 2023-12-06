ce3d91215aba17b5684c96da13370c5c5158b4b3
Mohit-TekwaniTR <112473013+Mohit-TekwaniTR@users.noreply.github.com>
2023-12-06 15:21:19 +0530

    Update setup-tasks.sh

--- .devcontainer/setup-tasks.sh
+++ .devcontainer/setup-tasks.sh
@@ -1,2 +1,35 @@
+#!/bin/bash
 
-code --goto "workspace/../content/event_bus.md"  
+mkdir -p .vscode
+cat <<EOL >.vscode/tasks.json
+{
+    "version": "2.0.0",
+    "tasks": [
+        {
+            "label": "openModel",
+            "type": "shell",
+            "command": "bash",
+            "args": [
+                "-x",
+                ".devcontainer/setup-tasks.sh"
+            ],
+            "group": {
+                "kind": "build",
+                "isDefault": true
+            },
+            "runOptions": {
+                "runOn": "folderOpen"
+            }
+        }
+    ]
+}
+EOL
+
+cat <<EOL >.vscode/settings.json
+{
+    "task.allowAutomaticTasks": "on"
+}
+EOL
+
+FILE_TO_OPEN="workspace/../content/event_bus.md"
+code --goto "$FILE_TO_OPEN"
